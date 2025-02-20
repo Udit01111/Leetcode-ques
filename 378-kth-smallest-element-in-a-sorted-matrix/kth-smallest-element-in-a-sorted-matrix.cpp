1170bf8329202997ec3870f7c5aaf1c29ccae6ea
Udit <150335620+Udit01111@users.noreply.github.com>
2025-02-20 11:58:13 +0530

    Time: 4 ms (63.90%) | Memory: 18.7 MB (19.68%) - LeetSync

--- 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.cpp
+++ 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
-vector<int> flattenRowMajor(const vector<vector<int>>& matrix, int n) {
-    vector<int> flattened;
+vector<int> RowMajor(const vector<vector<int>>& matrix, int n) {
+    vector<int> row;
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            flattened.push_back(matrix[i][j]);
+            row.push_back(matrix[i][j]);
         }
     }
-    return flattened;
+    return row;
 }
 
     int kthSmallest(vector<vector<int>>& matrix, int k) {
         int n = matrix.size();
-        vector<int> rowMajor = flattenRowMajor(matrix, n);
+        vector<int> rowMajor = RowMajor(matrix, n);
         sort(rowMajor.begin(),rowMajor.end());
         return rowMajor[k-1];
     }
