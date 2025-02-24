0d8cb5ff66296a0abe5086edaece3b355c3ffc88
Udit <150335620+Udit01111@users.noreply.github.com>
2025-02-24 07:09:17 +0530

    Time: 0 ms (100.00%) | Memory: 11 MB (43.93%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
   void postorder(TreeNode* root,vector<int> &ans){
         if(root==NULL) return;
-        postorder(root->left,ans);
+        postorder(root->left, ans) ;
     
-        postorder(root->right,ans);
+        postorder(root->right, ans) ;
 
         ans.push_back(root->val);
     }
