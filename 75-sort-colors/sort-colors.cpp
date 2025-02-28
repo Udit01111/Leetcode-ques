2de51c99798cc190a1a2f77851a64a46187800d0
Udit <150335620+Udit01111@users.noreply.github.com>
2025-02-28 15:16:36 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (5.54%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -4,16 +4,17 @@ public:
         int zero=0;
         int one=0;
         int two=0;
+
         for(int i=0;i<nums.size();i++){
-            if(nums[i]==0) zero++;
-            if(nums[i]==1) one++;
-            if(nums[i]==2) two++;
+            if(nums[i]==0) {zero++;}
+            if(nums[i]==1) {one++;}
+            if(nums[i]==2) {two++;}
         }
 
         for(int i=0;i<nums.size();i++){
-            if(i<zero){ nums[i]=0;}
-            else if(i< (zero + one)){ nums[i]=1; }
-            else {nums[i]=2;}
+            if(i<zero){nums[i]=0;}
+            else if(i<zero+one){nums[i]=1;}
+            else{nums[i]=2;}
         }
     }
 };
