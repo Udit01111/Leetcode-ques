ea6ee2677c3986734ab506128bef32366ca1f951
Udit <150335620+Udit01111@users.noreply.github.com>
2025-05-18 20:13:52 +0530

    Time: 0 ms (100.00%) | Memory: 62.9 MB (78.66%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.cpp
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -1,20 +1,28 @@
 class Solution {
 public:
-   int maxArea(vector<int>& height) {
-    int left = 0, right = height.size() - 1;
-    int maxWater = 0;
+    int maxArea(vector<int>& height) {
+        int ans=0;
+        int n=height.size();
+       
+        int left=0;
+        int right=n-1;
 
-    while (left < right) {
-        int h = min(height[left], height[right]);
-        int width = right - left;
-        maxWater = max(maxWater, h * width);
+        while(left<right){
+            int h= min(height[left],height[right]);
+            int width= right-left ;
+            ans= max(ans, h*width);
 
-        if (height[left] < height[right])
-            left++;
-        else
-            right--;
+            if(height[left]<height[right]){
+                left++;
+            }
+
+            else{
+                right--;
+            }
+        }
+        
+
+        return ans;
     }
-
-    return maxWater;
-}
 };
+
